3196da009755d240370df52e66f2a6967e26d3ec
CodeMuscle <investportal99@gmail.com>
2024-10-09 09:36:26 +0530

    List Panel

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
-// import List from "../ListView";
+import List from "../ListView";
 // import Timeline from "../TimelineView";
 // import Table from "../TableView";
 // import ModalNewTask from "@/components/ModalNewTask";
@@ -28,13 +28,13 @@ const Project = ({ params }: Props) => {
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
-      {/* {activeTab === "List" && (
+      {activeTab === "List" && (
         <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
-      {activeTab === "Timeline" && (
+      {/* {activeTab === "Timeline" && (
         <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Table" && (
+      )} */}
+      {/* {activeTab === "Table" && (
         <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )} */}
     </div>
